2b1c8581cbd88d4e397f2b6f0896985832ec2bab
theremias <104557989+theremias@users.noreply.github.com>
2024-06-12 19:47:34 +0200

    added test data

--- docs/source/firstDraft.py
+++ docs/source/firstDraft.py
@@ -1,61 +1,56 @@
-def add_join_csv_file(self, target_layer: QgsVectorLayer, target_field: str = "DruhPozemkuKod", csv_name: str = "druh_pozemku.csv", csv_field: str = "KOD", csv_subset: list = ["NAZEV"]) -> QgsVectorLayer:
-    """Joins a .csv (code list of land types with description) to layer 'Parcely'
-    """
-    path = os.path.join(os.path.dirname(__file__), "files", csv_name)
-    csv_layer = QgsVectorLayer(path, "DruhPozemku", "delimitedtext")
+import os
+from qgis.core import QgsProject, QgsVectorLayer, QgsVectorLayerJoinInfo
 
+def add_join_csv_file(target_layer: QgsVectorLayer, csv_layer: QgsVectorLayer, target_field: str = "DruhPozemkuKod", csv_field: str = "KOD", csv_subset: list = ["NAZEV"]) -> QgsVectorLayer:
+    """Performs left outer join from 'target_layer' to 'csv_layer'. Initially used to perform a join from 'Parcely' to code list of land types.
+    
+    @type target_layer: QgsVectorLayer
+    @param target_layer: A layer on which the join will be performed
+    @type csv_layer: QgsVectorLayer
+    @param csv_layer: A layer which will be joined to the 'target_layer'
+    @type target_field: str
+    @param target_field: Name of the field of the 'target_layer' that will be used for join
+    @type csv_field: str
+    @param csv_field: Name of the field of the 'csv_layer' that will be used for join
+    @type csv_subset: list
+    @param csv_subset: Subset of fields to be used from joined layer
+    
+    @rtype: QgsVectorLayer
+    @returns: A vector layer ('target_layer') with left outer join to other vector layer ('csv_layer') 
+    """
     joinObject = QgsVectorLayerJoinInfo()
     joinObject.setJoinLayerId(csv_layer.id())
     joinObject.setJoinFieldName(csv_field)
     joinObject.setTargetFieldName(target_field)
     joinObject.setUsingMemoryCache(True)
+    joinObject.setPrefix("")
     joinObject.setJoinLayer(csv_layer)
     joinObject.setJoinFieldNamesSubset(csv_subset)
 
     target_layer.addJoin(joinObject)
 
     return target_layer 
+    
+
+def rearrange_columns(self, layer: QgsVectorLayer, new_order: list) -> None:
     # rearrange columns: https://gis.stackexchange.com/a/445885 (ALE POTREBUJI CELEJ LIST SLOUPCU ASI)
-    # import csv to SQLite: https://www.geeksforgeeks.org/how-to-import-a-csv-file-into-a-sqlite-database-table-using-python/
-
-
-def importCsv2Sqlite(self, data):
-    import csv, sqlite3
-    con = sqlite3.connect(":memory:") # QUESTION: asi bude potreba vytvorit (novou) databazi nejdriv?
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE druh_pozemku (
-                    KOD int primary key,
-                    NAZEV text, 
-                    ZEMEDELSKA_KULTURA varchar(1), 
-                    PLATNOST_OD text, 
-                    PLATNOST_DO text, 
-                    ZKRATKA text, 
-                    TYPPPD_KOD int, 
-                    STAVEBNI_PARCELA varchar(1), 
-                    POVINNA_OCHRANA_POZ int, 
-                    POVINNY_ZPUSOB_VYUZ varchar(1));""")
-    with open(data, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=';') #comma is default delimiter
-        to_db = [(i["KOD"], i["NAZEV"], i["ZEMEDELSKA_KULTURA"], i["PLATNOST_OD"], i["PLATNOST_DO"], i["ZKRATKA"], i["TYPPPD_KOD"], i["STAVEBNI_PARCELA"], i["POVINNA_OCHRANA_POZ"], i["POVINNY_ZPUSOB_VYUZ"]) for i in reader] #QUESTION: dlouhy jaxvist, jak na vic radku?
-
-    cur.executemany("""INSERT INTO druh_pozemku (
-                        KOD, 
-                        NAZEV, 
-                        ZEMEDELSKA_KULTURA, 
-                        PLATNOST_OD, 
-                        PLATNOST_DO, 
-                        ZKRATKA, 
-                        TYPPPD_KOD, 
-                        STAVEBNI_PARCELA, 
-                        POVINNA_OCHRANA_POZ, 
-                        POVINNY_ZPUSOB_VYUZ) 
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""", to_db)
-    con.commit()
-    con.close()
+    pass
 
 def main():
-    uri = "Parcely.gpkg"
-    add_join_csv_file(QgsVectorLayer(uri, "Parcely2", "ogr"))
+    uri ="c:\\Users\\Vojta\\_CVUT\\2023_2024\\FGIS\\ruianTest.gpkg|layername=parcely"
+    parcely_layer = QgsVectorLayer(uri, "parcely", "ogr")
+    parcely_field = "DruhPozemkuKod"
 
-if __name__ == "__main__":
+    # headTail = os.path.split(os.path.dirname(__file__)) # TODO: po zarazeni do MainApp upravit cestu
+    path = "file:///c:\\Users\\Vojta\\_CVUT\\2023_2024\\FGIS\\data\\SC_D_POZEMKU.csv?delimiter=;"
+    csv_layer = QgsVectorLayer(path, "DruhPozemku", "delimitedtext")
+    csv_field = "KOD"
+
+
+
+    mylayer = add_join_csv_file(parcely_layer, csv_layer, parcely_field, csv_field)
+    QgsProject.instance().addMapLayer(mylayer)
+    new_order = []
+
+if __name__ in ('__main__', '__script__', '__console__'):
     main()       
