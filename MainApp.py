7f6eb5ab5a1cb2b6c39949a26caee7065526e018
theremias <104557989+theremias@users.noreply.github.com>
2024-06-24 12:06:19 +0200

    metoda 'add_join' zacelnena do pluginu a volana v metode 'add_layers'

--- MainApp.py
+++ MainApp.py
@@ -30,7 +30,7 @@ from qgis.PyQt.QtCore import QSortFilterProxyModel, QThread, pyqtSignal, qDebug,
 from qgis.PyQt.QtGui import QStandardItem, QColor, QStandardItemModel
 from qgis.PyQt.QtWidgets import QDialog, QAbstractItemView, QFileDialog, QProgressDialog, QMessageBox, QLineEdit
 
-from qgis.core import QgsProject, QgsVectorLayer, Qgis, QgsMessageLog, QgsProcessingUtils, QgsCoordinateReferenceSystem
+from qgis.core import QgsProject, QgsVectorLayer, Qgis, QgsMessageLog, QgsProcessingUtils, QgsCoordinateReferenceSystem, QgsVectorLayerJoinInfo
 
 from osgeo import ogr, gdal
 
@@ -450,6 +450,41 @@ class MainApp(QDialog):
         elif i == 1 and self.ui.selectionComboBox.isEnabled():
             self.ui.selectionComboBox.setEnabled(False)
 
+    def add_join(self,
+                 target_layer: QgsVectorLayer, 
+                 csv_layer: QgsVectorLayer, 
+                 target_field: str = "DruhPozemkuKod", 
+                 csv_field: str = "KOD", 
+                 csv_subset: list = ["NAZEV"]) -> QgsVectorLayer:
+        """Performs left outer join from 'target_layer' to 'csv_layer'. Initially used to perform a join from 'Parcely' to code list of land types.
+        
+        :type target_layer: QgsVectorLayer
+        :param target_layer: A layer on which the join will be performed
+        :type csv_layer: QgsVectorLayer
+        :param csv_layer: A layer which will be joined to the 'target_layer'
+        :type target_field: str
+        :param target_field: Name of the field of the 'target_layer' that will be used for join
+        :type csv_field: str
+        :param csv_field: Name of the field of the 'csv_layer' that will be used for join
+        :type csv_subset: list
+        :param csv_subset: Subset of fields to be used from joined layer
+        
+        :rtype: QgsVectorLayer
+        :returns: A vector layer ('target_layer') with left outer join to other vector layer ('csv_layer') 
+        """
+        joinObject = QgsVectorLayerJoinInfo()
+        joinObject.setJoinLayerId(csv_layer.id())
+        joinObject.setJoinFieldName(csv_field)
+        joinObject.setTargetFieldName(target_field)
+        joinObject.setUsingMemoryCache(True)
+        joinObject.setPrefix("")
+        joinObject.setJoinLayer(csv_layer)
+        joinObject.setJoinFieldNamesSubset(csv_subset)
+
+        target_layer.addJoin(joinObject)
+
+        return target_layer 
+    
     def add_layers(self):
         """Add created layers to map display.
         """
@@ -517,6 +552,16 @@ class MainApp(QDialog):
                 if not add_layer(layerGroup, layer, layer_alias):
                     QgsMessageLog.logMessage('RUIAN plugin: prázdná vrstva "{}" přeskočena ({})'.format(
                         layer_name, self.option['datasource']), level=Qgis.Info)
+
+        parcely_layer = QgsProject.instance().mapLayersByName('parcely')[0]
+        parcely_field = "DruhPozemkuKod"
+        csv_path = os.path.join(os.path.dirname('__file__'), "files", "druh_pozemku.csv")
+        csv_layer = QgsVectorLayer(f"file:///{csv_path}?delimiter=;", "DruhPozemku", "delimitedtext")
+        csv_field = "KOD"
+        QgsProject.instance().addMapLayer(csv_layer, addToLegend=False)
+        
+        add_join(parcely_layer, csv_layer, parcely_field, csv_field)
+        
 
         del datasource # close datasource
 
